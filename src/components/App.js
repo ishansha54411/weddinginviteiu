f8a05a153df48236d3ccf0e449c80aafe52bd479
ishansha54411 <ishan98.es@gmail.com>
2023-04-30 18:11:59 +0530

    React Router Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { createBrowserRouter, RouterProvider} from "react-router-dom";
+// import { createBrowserRouter, RouterProvider} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ForgotPassword from "../pages/ForgotPassword";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -8,37 +9,46 @@ import UpdatePassword from "../pages/UpdatePassword";
 import VerifyEmail from "../pages/VerifyEmail";
 
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    }
-    //,
-    // {
-    //   path:"/login",
-    //   element:<Login/>
-    // },
-    // {
-    //   path:"/register",
-    //   element:<RegisterUser/>
-    // },
-    // {
-    //   path:"/verify-email",
-    //   element:<VerifyEmail/>
-    // },
-    // {
-    //   path:'/forgot-password',
-    //   element:<ForgotPassword/>
-    // },
-    // {
-    //   path:'/update-password',
-    //   element:<UpdatePassword/>
-    // }
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home/>,
+  //   },
+  //   {
+  //     path:"/login",
+  //     element:<Login/>
+  //   },
+  //   {
+  //     path:"/register",
+  //     element:<RegisterUser/>
+  //   },
+  //   {
+  //     path:"/verify-email",
+  //     element:<VerifyEmail/>
+  //   },
+  //   {
+  //     path:'/forgot-password',
+  //     element:<ForgotPassword/>
+  //   },
+  //   {
+  //     path:'/update-password',
+  //     element:<UpdatePassword/>
+  //   }
+  // ]);
   
 const App=()=>{
     return (
-        <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<RegisterUser/>}/>
+          <Route path="/verify-email" element={<VerifyEmail/>}/>
+          <Route path="/forgot-password" element={<ForgotPassword/>}/>
+          <Route path="/update-password" element={<UpdatePassword/>}/>
+        </Routes>
+      </BrowserRouter>
+        // <RouterProvider router={router} />
       )
 }
 
